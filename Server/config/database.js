0ee57799c2a739c99f8b913d59427ccccea97bd1
Sarthakd43 <140257318+Sarthakd43@users.noreply.github.com>
2024-07-29 21:37:03 +0530

    first commit

---
+++ Server/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+exports.connectDB = () => {
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("DB connection successfull!")
+    })
+    .catch( (error) => {
+        console.log("DB Connection Failed");
+        console.error(error);
+        process.exit(1);
+    } )
+}
